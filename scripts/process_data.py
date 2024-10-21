022a0cdcc6dd8d5cb0ccbda9c1ed6bbf415dea6c
Siddharth Joshi <sjoshi804@gmail.com>
2024-10-21 15:45:09 -0700

    process data

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -2,14 +2,14 @@ import json
 import os
 import sys
 
-def process_json(json_file_path, output_dir):
+def process_json(json_file_path, input_dir):
     # Read the JSON file
     with open(json_file_path, 'r') as file:
         data = json.load(file)
     
-    # Prepend the OUTPUT_DIR environment variable to the image_folder attribute
+    # Prepend the input_dir to the image_folder attribute
     if 'image_folder' in data:
-        data['image_folder'] = os.path.join(output_dir, data['image_folder'])
+        data['image_folder'] = os.path.join(input_dir, data['image_folder'])
     else:
         raise ValueError("JSON file does not contain 'image_folder' attribute")
     
@@ -19,9 +19,9 @@ def process_json(json_file_path, output_dir):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python process_data.py <json_file_path> <output_dir>")
+        print("Usage: python process_data.py <json_file_path> <input_dir>")
         sys.exit(1)
     
     json_file_path = sys.argv[1]
-    output_dir = sys.argv[2]
-    process_json(json_file_path, output_dir)
+    input_dir = sys.argv[2]
+    process_json(json_file_path, input_dir)
