38a28d399e46856cd3570ef0fc41cf4a2b8838d2
Siddharth Joshi <sjoshi804@gmail.com>
2024-10-31 15:04:41 -0700

    longer training for ai2d

---
+++ scripts/vibhav_run/10_31_2024_ai2d_mm_instruct_filter_3x.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+TRAIN_DATA_PATH=$INPUT_DIR/generated_data/ai2d_mminstruct_filtered_50k.json
+RUN_ID=ai2d_mminstruct_filtered_50k_3x_10_31_2024
+
+python scripts/process_data.py $TRAIN_DATA_PATH $INPUT_DIR
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path liuhaotian/llava-v1.5-7b \
+    --version $RUN_ID \
+    --data_path $TRAIN_DATA_PATH \
+    --image_folder ./data_images \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir $OUTPUT_DIR/checkpoints/llava_$RUN_ID \
+    --num_train_epochs 6 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "epoch" \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
