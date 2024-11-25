0a162cb93add413cb9c07dea9c6a97205d3a8a0b
Siddharth Joshi <sjoshi804@gmail.com>
2024-11-25 12:16:22 -0800

    Update 11_13_2024_all_tasks_merged.sh

--- scripts/vibhav_run/11_13_2024_all_tasks_merged.sh
+++ scripts/vibhav_run/11_13_2024_all_tasks_merged.sh
@@ -20,13 +20,13 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed llava/train/train_mem.py \
     --group_by_modality_length True \
     --bf16 True \
     --output_dir $OUTPUT_DIR/checkpoints/llava_$RUN_ID \
-    --num_train_epochs 3 \
+    --num_train_epochs 8 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 5000 \
+    --save_steps 50000 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
